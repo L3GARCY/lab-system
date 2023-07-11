dad3fde9f9c0c341870acb71e3b66a616d63709e
Trevor Malesi <malesitrevorke@proton.me>
2023-07-11 09:48:11 +0300

    added a modal to add items

--- authentication/models.py
+++ authentication/models.py
@@ -13,3 +13,32 @@ class CustomUser(models.Model):
 
     def __str__(self):
         return self.username
+class CustomItem(models.Model):
+    LAB=(
+        ("LBB-011","LBB-011"),
+        ("LBB-015","LBB-015"),
+        ("LBB-016","LBB-016"),
+    )
+    ITEMS=(
+        ("projector","projector"),
+        ("keyboard","keyboard"),
+        ("mouse","mouse"),
+        ("monitor","monitor"),
+        ("system unit","system unit"),
+        ("printer","printer"),
+        ("networking toolkits","networking toolkit"),
+        ("extension cable","extension cable"),
+    )
+    CONDITION=(
+        ("Working","Working"),
+        ("Faulty","Faulty"),
+        ("Damaged","Damaged"),
+    )
+    item_name=models.CharField(max_length=50,choices=ITEMS)
+    serial_number=models.CharField(max_length=50)
+    condition=models.CharField(max_length=50,choices=CONDITION)
+    lab=models.CharField(max_length=50,choices=LAB)
+    date_added=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.item_name
+    
